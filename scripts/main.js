7cb5e5dd8287c210f1ae600237cee016fccb9f7f
winnie <asgh03226@gmail.com>
2020-12-24 01:33:43 +0800

    2TODO unfinished

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,83 @@ document.addEventListener('DOMContentLoaded',() => {
 
   const cartDelBtn = document.querySelectorAll('.cart_del')
   const addQuantity = document.querySelectorAll('.cart .quantity')
+  const cartItems = document.querySelectorAll('.add_product')
+
 
   cartDelBtn.forEach( btn => { btn.addEventListener('click',delItem) })
   addQuantity.forEach(input => { input.addEventListener('change', setQuantity)})
+  cartItems.forEach((btn) => {btn.addEventListener('click', addItem)})
 
-  
-
-  
   
 })
+
+function addItem(e) {
+
+  const product = e.currentTarget.parentElement.parentElement
+  
+  const price = product.querySelector('.price').innerText.replace('$','')
+  const productName = product.querySelector('.product_name').innerText
+
+  let  cartList = document.querySelector('.cart-list')
+
+  let tr= document.createElement('tr')
+  
+  tr.classList.add('cart-item')
+
+  tr.innerHTML=
+    `
+    <td>${productName}</td>
+    <td><input type="number" value="2" class="quantity"></td>
+    <td class="price">${price}</td>
+    <td class="subTotal">$25</td>
+    <td><button class="cart_del btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button></td>
+     
+    `
+  const  items = cartList.querySelectorAll('.cart-item')
+  for(let i = 0; i< items.length; i++){
+    
+    const item = items[i]
+
+    //TODO
+
+    let title = item.querySelector('.item-name').innerText
+
+    
+
+    if(title === productName){
+
+      item.querySelector('.quantity').value = Number(item.querySelector('.quantity').value)+1
+
+      // return;
+
+    }
+
+
+        
+
+
+  }
+    cartList.appendChild(tr)
+    
+    
+
+
+
+  //  if(productName ===)
+    
+
+
+
+
+  
+
+  
+
+  
+
+
+  
+}
 
 
 function delItem(e){
@@ -43,3 +111,12 @@ function setQuantity(e) {
 
 }
 
+
+
+// function updatePrice() {
+
+//TODO
+
+
+  
+// }
